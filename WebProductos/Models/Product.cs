e7b7b8d7ec25e86ff0e16a07f5d9514ddfcf24e0
Sebastian Katz <sebikatzcatz@gmail.com>
2021-11-11 18:51:29 -0300

    Add project files.

---
+++ WebProductos/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebProductos.Models
+{
+    public class Product
+    {
+        [Key()]
+        public int Id { get; set; }
+        public string Category { get; set; }
+
+        [DisplayName("Name")]
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+
+        [DisplayName("Available Date")]
+        public DateTime AvailableDate { get; set; }
+    }
+}
---
+++ WebProductos/Models/ProductDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebProductos.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext() : base("KeyProducts") { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
